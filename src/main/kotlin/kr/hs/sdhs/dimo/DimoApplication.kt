0e18c152eeb7d6cf0a534faaf6a019e04e19679b
balhyo-younjisang <yjs12180825@gmail.com>
2025-03-20 03:13:14 +0900

    Initialized Repository

---
+++ src/main/kotlin/kr/hs/sdhs/dimo/DimoApplication.kt
@@ -0,0 +1,11 @@
+package kr.hs.sdhs.dimo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DimoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DimoApplication>(*args)
+}
---
+++ src/test/kotlin/kr/hs/sdhs/dimo/DimoApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.hs.sdhs.dimo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DimoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
